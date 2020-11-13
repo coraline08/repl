57adc194859cec71a72d579423fa394fa6034b05
repl.it user <replituser@example.com>
2020-11-13 16:15:35 +0000

    somma

--- Intro.py
+++ Intro.py
@@ -1,3 +1,14 @@
 print("Hello world!")
 
 
+def list_sum(the_list):
+    sum=0
+    for item in the_list:
+        sum = sum + item
+    print("Somma:{}".format(sum))
+list_sum([1,4,10])
+
+
+
+
+    
